6b2ddb5c7cbcf86eb6fe7ae22e0a431c39ed6f4b
Eileen Reid <eileenreid4657@gmail.com>
2020-03-31 14:39:46 -0400

    Updating files

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// create our Example model
+class User extends Model {}
+
+// create fields/columns for Post model
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    sex: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },
+  {
+    sequelize
+  }
+);
+
+module.exports = User;
